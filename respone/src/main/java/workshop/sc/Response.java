0d8ce96c9dd2e8c1d3bd3cb15fae82c35b08836d
Lukasz <my.email>
2020-09-01 21:03:34 +0200

    Krok 1  - Response

---
+++ respone/src/main/java/workshop/sc/Response.java
@@ -0,0 +1,16 @@
+package workshop.sc;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Response {
+
+    private String service;
+    private String msg;
+
+}
+
